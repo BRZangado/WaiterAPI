a26f23ce36039ed7474f2f59de3380293aace6ea
Bruno Rodrigues <bruesmanbruesman@hotmail.com>
2018-10-18 14:53:55 -0300

    inital setup

---
+++ src/main/java/com/example/waiter/WaiterPremiumApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.waiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaiterPremiumApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaiterPremiumApiApplication.class, args);
+	}
+}
